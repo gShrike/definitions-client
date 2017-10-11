c86e4fece6312f23f976f8edd99afe0a59010835
Aaron Goodman <amgoodman5@gmail.com>
2017-10-11 14:09:58 -0600

    added list and single terms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Single from './components/Single';
+import List from './components/List'; // import Header from 'header.js'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -8,8 +11,15 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Definitions...Let us define you ..you</h2>
         </div>
+
+        <Router>
+          <div>
+            <Route exact path="/" component={List} />
+            <Route exact path="/terms/:id" component={Single} />
+          </div>
+        </Router>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>

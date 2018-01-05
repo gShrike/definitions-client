10b9aef342b450fbc556783588cdf6b29303b708
chadwithuhc <chadwithuhc@gmail.com>
2018-01-05 14:45:05 -0700

    Cleanup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const navLinks = [
-  { name: `Terms`, uri: `/terms` },
-  { name: `Categories`, uri: `/categories` },
-  { name: `Questions`, uri: `/questions` },
+  { name: `Terms`, uri: `/terms/` },
+  { name: `Categories`, uri: `/categories/` },
+  { name: `Questions`, uri: `/questions/` },
 
 ]
 
 class Navbar extends React.Component {
 
   render() {
-    console.log(this.props.match)
     return (
       <nav className="navbar is-light">
         <div className="tabs is-centered is-medium">
@@ -22,17 +21,6 @@ class Navbar extends React.Component {
               return <li key={link.name} className={activeClass}><Link to={link.uri}>{link.name}</Link></li>
             })}
           </ul>
-        </div>
-
-        <div className="navbar-start">
-          {/*
-
-          <a className="navbar-item" href="/categories">
-            Categories
-          </a>
-          <a className="navbar-item" href="/questions">
-            Questions
-          </a> */}
         </div>
       </nav>
     )

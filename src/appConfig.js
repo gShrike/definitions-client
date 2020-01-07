335f43f6fa98df0b4ce511e5a42f392f6e67c9c5
chadwithuhc <chadwithuhc@gmail.com>
2020-01-06 17:16:47 -0700

    Move naming config to env file

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,7 +1,7 @@
 export default {
 
-  BOOK_NAME: 'Dev Book',
-  BOOK_ICON: 'fa-terminal', // from fontawesome v4.7
+  BOOK_NAME: process.env.REACT_APP_BOOK_NAME,
+  BOOK_ICON: process.env.REACT_APP_BOOK_ICON, // from fontawesome v4.7
   API_URL: process.env.REACT_APP_API_URL
 
 }

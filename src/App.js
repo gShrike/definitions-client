6fedbe4907b56883ebca74d38e03a5c829d701f0
chadwithuhc <chadwithuhc@gmail.com>
2020-04-19 17:30:54 -0600

    Redirect to Books from Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import Books from './components/books/index'
 import Login from './components/Login'
 import Auth from './components/Auth'
+
+const HomepageRedirect = () => <Redirect to="/books" />
 
 class App extends Component {
   render() {
@@ -12,6 +14,7 @@ class App extends Component {
         <Router>
           <Route path="/auth/callback" component={Auth} />
           <Route path="/" component={Login} />
+          <Route exact path="/" component={HomepageRedirect} />
           <Route exact path={`/${Books.DataStore.uri}`} component={Books.List} />
           <Route exact path={`/${Books.DataStore.uri}/new`} component={Books.Form} />
           <Route exact path={`/${Books.DataStore.uri}/:bookId/:any?/:any?`} component={Books.Single} />
